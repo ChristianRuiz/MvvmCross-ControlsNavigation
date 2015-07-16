b885806666537935be6ab4c9025ec62fbee3bf09
Patrick Lind <patrick.lind@producepro.com>
2015-07-16 12:11:10 -0400

    Updating to MvvmCross 3.5.1

--- MupApps.MvvmCross.Plugins.ControlsNavigation/Sample/MupApps.ControlsNavigation.Sample.Wpf/DebugTrace.cs
+++ MupApps.MvvmCross.Plugins.ControlsNavigation/Sample/MupApps.ControlsNavigation.Sample.Wpf/DebugTrace.cs
@@ -24,7 +24,7 @@ namespace MupApps.ControlsNavigation.Sample.Wpf
             }
             catch (FormatException)
             {
-                Trace(MvxTraceLevel.Error, tag, "Exception during trace of {0} {1} {2}", level, message);
+                Trace(MvxTraceLevel.Error, tag, "Exception during trace of {0} {1}", level, message);
             }
         }
     }
